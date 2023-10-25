58611a827241005fb6e532e7968874c9d37a8a7f
pxltr <tymon.zaniewski.pl@gmail.com>
2023-10-25 03:31:15 +0200

    moved api mockup to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,36 @@
-export const WEBSITE_NAME = 'tymon_zaniewski'
+export const WEBSITE_NAME = 'tymon zaniewski'
+
+export const API_MOCK = {
+    projects: [
+        {
+            name: 'Lust EP',
+            date: '2024',
+            tags: ['sound', 'er255', 'album'],
+            image: 'lust.png'
+        },
+        {
+            name: 'Technobioform',
+            date: '2023',
+            tags: ['er255', 'sound', 'visuals'],
+            image: 'frame.png'
+        },
+        {
+            name: 'Memory Error',
+            date: '2023',
+            tags: ['sound', 'installation', 'electronics'],
+            image: 'bg2.jpg'
+        },
+        {
+            name: 'Rave Bioflyers',
+            date: '2023',
+            tags: ['graphics', 'biomaterials', 'research'],
+            image: 'flyer.png'
+        },
+        {
+            name: 'CRTotem',
+            date: '2022',
+            tags: ['sound', 'installation', 'graphics'],
+            image: 'crtotem.jpg'
+        },
+    ]
+}
