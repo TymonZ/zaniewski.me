b2bac7caa01505d3bc7a2ca3e52c68696e538500
pxltr <tymon.zaniewski.pl@gmail.com>
2023-11-05 01:11:46 +0100

    added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), compress()],
   output: "server",
   adapter: vercel()
 });
